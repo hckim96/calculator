0b3aea804737d4c38c8302630366615daec6ea50
hckim96 <hyeoncheolkim96@gmail.com>
2020-11-10 16:15:56 +0900

    WIP

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,52 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {View, Text, Button, FlatList} from 'react-native';
 import {HomeHeader} from '../components/HomeHeader.js';
 import {ProfileCard} from '../components/ProfileCard.js';
-let NumOfDays = 30;
 // 0 ~ 2 : 408100 , 3 ~ 8 : 441700, 9 ~ 14 : 488200, 15 ~ : 540900  2, 6, 6, 나머지 개월
-let money = 441700;
-
-let additionalPerDays = 8500;
-let additionalMoney = additionalPerDays * (20 + 10.0 / 7);
-
-let now = new Date();
-let dischargeDay = new Date('November 3, 2021 15:00:00');
-let beginDay = new Date('January 29, 2020 15:00:00');
-
-const leftDays = parseInt((dischargeDay - now) / (1000 * 60 * 60 * 24));
-const passedDays = Math.ceil((now - beginDay) / (1000 * 60 * 60 * 24));
-const total = parseInt((dischargeDay - beginDay) / (1000 * 60 * 60 * 24));
-
-
-const dday = [
-  {
-    dayName: '전체 복무일',
-    dayNumber: total,
-  },
-  {
-    dayName: '현재 복무일',
-    dayNumber: passedDays,
-  },
-  {
-    dayName: '남은 복무일',
-    dayNumber: leftDays,
-  },
-  {
-    dayName: '다음 진급일',
-    dayNumber: 162,
-  },
-];
 
 export const HomeScreen = ({navigation}) => {
-  const [difference, setDifference] = useState();
   const BASE_PAY = [408100, 441700, 488200, 540900];
+  let now = new Date();
   let transportationExpenses = 2800;
   let foodExpenses = 6000;
-  let dischargeDay = new Date('November 3, 2021 15:00:00');
-  let beginDay = new Date('January 29, 2020 15:00:00');
-  let militaryClass = getMilitaryClass();
-  // let workingDays = getWorkingDay();
-  let now = new Date();
+  let beginDay = new Date('January 30, 2020');
+  let dischargeDay = new Date(beginDay);
+  dischargeDay.setDate(dischargeDay.getDate() + 643);
+  
+  let militaryClass = getMilitaryClass(new Date(2020, 2));
+  const leftDays = parseInt((dischargeDay - now) / (1000 * 60 * 60 * 24)) + 1;
+  const passedDays = Math.ceil((now - beginDay) / (1000 * 60 * 60 * 24));
+  const total = parseInt((dischargeDay - beginDay) / (1000 * 60 * 60 * 24));
+
+  const [passed_pct, setPassed_pct] = useState(((new Date() - beginDay) / (dischargeDay - beginDay)) * 100);
+  // function useInterval(callback, delay) {
+  //   const savedCallback = useRef();
+  
+  //   // Remember the latest callback.
+  //   useEffect(() => {
+  //     savedCallback.current = callback;
+  //   }, [callback]);
+  
+  //   // Set up the interval.
+  //   useEffect(() => {
+  //     function tick() {
+  //       savedCallback.current();
+  //     }
+  //     if (delay !== null) {
+  //       let id = setInterval(tick, delay);
+  //       return () => clearInterval(id);
+  //     }
+  //   }, [delay]);
+  // }
+
+  // useInterval(() => {
+  //   // now = new Date();
+  //   setPassed_pct(((new Date() - beginDay) / (dischargeDay - beginDay)) * 100);
+  // }, 1000 / 30);
+  const dday = [
+    {
+      dayName: '전체 복무일',
+      dayNumber: total,
+    },
+    {
+      dayName: '현재 복무일',
+      dayNumber: passedDays,
+    },
+    {
+      dayName: '남은 복무일',
+      dayNumber: leftDays,
+    },
+    // {
+    //   dayName: '다음 진급일',
+    //   dayNumber: 162,
+    // },
+  ];
   function getWorkingDay(monthIndex) {
     let total =  new Date(2020, monthIndex + 1, 1) - new Date(2020, monthIndex, 1);
     total = total / (1000 * 60 * 60 * 24);
@@ -64,6 +78,7 @@ export const HomeScreen = ({navigation}) => {
   }
 
   function getMilitaryClass() {
+    
     let now = new Date();
     let nowMonth;
     if (now.getFullYear() === beginDay.getFullYear()) {
@@ -84,21 +99,25 @@ export const HomeScreen = ({navigation}) => {
   }
 
   function getTotalPay() {
-
-    // should check military class 
-    // to get month parameter
-    
+    let militaryClass = getMilitaryClass();
     let workingDays = getWorkingDay(now.getMonth());
     let totalPay = BASE_PAY[militaryClass] + workingDays * (foodExpenses + transportationExpenses);
-    return `${parseInt(totalPay / 1000)},${
-      parseInt(totalPay) % 1000 == 0 ? "000" : parseInt(totalPay) % 1000
-    }`
-    
+
+    return (
+      <View >
+        <Text style = {{textAlign: "center", fontSize: 20, fontWeight: 'bold'}}>
+        {`${parseInt(totalPay / 1000)},${parseInt(totalPay) % 1000 == 0 ? "000" : parseInt(totalPay) % 1000}`}
+        </Text>
+        <Text style={{fontSize: 15, fontWeight: 'bold', textAlign: "center"}}>
+        {`(${BASE_PAY[militaryClass]} + ${workingDays} * ${foodExpenses + transportationExpenses})`}
+        </Text>
+      </View>
+    )
   }
+
   return (
     <View style={{flexDirection: 'column', flex: 1}}>
       <HomeHeader navigation={navigation}></HomeHeader>
-
       <View
         style={{
           paddingHorizontal: 30,
@@ -115,21 +134,15 @@ export const HomeScreen = ({navigation}) => {
         <View
           style={{
             flex: 1,
-            // backgroundColor: "gray",
             alignItems: 'center',
             justifyContent: 'center',
+            
           }}>
-          <Text style={{fontSize: 15, fontWeight: 'bold'}}>D - {leftDays}</Text>
           <Text style={{fontSize: 55, fontWeight: 'bold'}}>
-            {`${parseInt((money + additionalMoney) / 1000)},${
-              parseInt(money + additionalMoney) % 1000
-            }`}
-          </Text>
-          <Text>
             {getTotalPay()}
           </Text>
-          <Text>
-            {getWorkingDay(now.getMonth())}
+          <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+            {`${passed_pct.toPrecision(10)}%`}
           </Text>
         </View>
         <View style={{bottom: 0}}>
